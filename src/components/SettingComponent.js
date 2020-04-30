d0ce54c03b4e5445f705c97bd421cd2465f9b036
saihaj <44710980+saihaj@users.noreply.github.com>
2020-04-30 15:59:00 -0500

    feat: exports for settings
    Change default exports for components and create  a color picker component

--- src/components/SettingComponent.js
+++ src/components/SettingComponent.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable object-curly-newline */
-import React from 'react'
+import React, { useContext } from 'react'
 import { string, func, any, arrayOf, number, bool } from 'prop-types'
 
 import classNames from 'classnames'
@@ -13,7 +13,10 @@ import {
   Button as MaterialButton,
 } from '@material-ui/core'
 
+import { SketchPicker } from 'react-color'
+
 import { OPTION_TYPES } from '../lib/options'
+import { SettingsContext } from '../lib/contexts'
 
 const generalPropTypes = {
   option: string.isRequired,
@@ -52,35 +55,62 @@ Toggle.propTypes = {
   value: bool.isRequired,
 }
 
-export const Slider = ( { value, ...props } ) => (
-  <MaterialSlider
-    className="slider"
-    valueLabelDisplay="auto"
-    value={value}
-    {...props}
-  />
-)
+export const Slider = ( { name, min, max, step, storageKey, units, ...props } ) => {
+  const [ settings, setSettings ] = useContext( SettingsContext )
+
+  return (
+    <MaterialSlider
+      className="slider"
+      valueLabelDisplay="auto"
+      value={settings[name] || window.localStorage.getItem( storageKey ).split( units )[0]}
+      min={min}
+      max={max}
+      step={step}
+      onChange={( _event, newValue ) => {
+        setSettings( { ...settings, [name]: newValue } )
+        document.documentElement.style.setProperty( storageKey, `${newValue}${units}` )
+        window.localStorage.setItem( storageKey, `${newValue}${units}` )
+      }}
+      {...props}
+    />
+  )
+}
 
 Slider.propTypes = {
-  value: number.isRequired,
+  name: string.isRequired,
+  min: number.isRequired,
+  max: number.isRequired,
+  step: number.isRequired,
+  storageKey: string.isRequired,
+  units: string.isRequired,
 }
 
-export const Dropdown = ( { value, values, onChange, ...props } ) => (
-  <Select className="select" MenuProps={{ className: 'select-menu' }} value={value} onChange={onChange} {...props}>
-    {values.map(
-      ( { name, value } ) => <MenuItem key={value} value={value}>{name || value}</MenuItem>,
-    )}
-  </Select>
-)
+export const Dropdown = ( { name, storageKey, values, ...props } ) => {
+  const [ settings, setSettings ] = useContext( SettingsContext )
+
+  return (
+    <Select
+      className="select"
+      MenuProps={{ className: 'select-menu' }}
+      value={settings[name] || window.localStorage.getItem( storageKey )}
+      onChange={event => {
+        setSettings( { ...settings, [name]: event.target.value } )
+        document.documentElement.style.setProperty( storageKey, event.target.value )
+        window.localStorage.setItem( storageKey, event.target.value )
+      }}
+      {...props}
+    >
+      {values.map(
+        ( { name, value } ) => <MenuItem key={value} value={value}>{name || value}</MenuItem>,
+      )}
+    </Select>
+  )
+}
 
 Dropdown.propTypes = {
-  value: any.isRequired,
+  name: string.isRequired,
+  storageKey: string.isRequired,
   values: arrayOf( any ).isRequired,
-  onChange: func,
-}
-
-Dropdown.defaultProps = {
-  onChange: () => {},
 }
 
 export const Button = ( { className, ...props } ) => (
@@ -99,10 +129,37 @@ Button.defaultProps = {
   className: null,
 }
 
+export const ColorPicker = ( { name, storageKey, ...props } ) => {
+  const [ settings, setSettings ] = useContext( SettingsContext )
+
+  return (
+    <SketchPicker
+      className="colorPicker"
+      color={settings[name] || window.localStorage.getItem( storageKey )}
+      onChange={color => {
+        const rgba = color.rgb
+        setSettings( { ...settings, [name]: color.rgb } )
+        document.documentElement.style.setProperty( storageKey, `rgba(${rgba.r},${rgba.g},${rgba.b},${rgba.a})` )
+      }}
+      onChangeComplete={color => {
+        const rgba = color.rgb
+        window.localStorage.setItem( storageKey, `rgba(${rgba.r},${rgba.g},${rgba.b},${rgba.a})` )
+      }}
+      {...props}
+    />
+  )
+}
+
+ColorPicker.propTypes = {
+  name: string.isRequired,
+  storageKey: string.isRequired,
+}
+
 const typeComponents = {
   [OPTION_TYPES.dropdown]: GeneralSettingEvent( Dropdown ),
   [OPTION_TYPES.toggle]: GeneralSettingParam( Toggle ),
   [OPTION_TYPES.slider]: GeneralSettingParam( Slider ),
+  [OPTION_TYPES.colorPicker]: GeneralSettingParam( ColorPicker ),
 }
 
 export default type => typeComponents[type]
