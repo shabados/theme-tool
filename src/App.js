ef67d861216288f3c5e93556861e7b34cc39a68b
saihaj <44710980+saihaj@users.noreply.github.com>
2020-06-15 14:49:15 -0500

    feat: error fallback component
    Modify the desktop component and implement it here
    
    Co-Authored-By: Harjot Singh <contact@harjot.me>
    Co-Authored-By: Bhajneet S.K. <bhajneet@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { SettingsContext } from './lib/contexts'
 import { loadCss, loadStorage, writeCss } from './lib/utils'
 import { OPTIONS, LOCK_PROPERTIES } from './lib/options'
 import MOOL_MANTAR from './lib/mool-mantar'
+import { withErrorFallback } from './components/ErrorFallback'
 
 import './App.css'
 
@@ -129,4 +130,4 @@ const App = () => {
 }
 
 
-export default App
+export default withErrorFallback( App )
