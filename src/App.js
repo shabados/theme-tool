ff908ba9942c693b0878b688e1ee0b358f59273c
ਸਹਜਪ੍ਰੀਤ ਸਿੰਘ <saihajpreet.singh@gmail.com>
2020-06-28 09:28:38 -0500

    feat: add error fallback component (#47)
    Co-authored-by: Harjot Singh <contact@harjot.me>
    Co-authored-by: Bhajneet S.K. <bhajneet@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { SettingsContext } from './lib/contexts'
 import { loadCss, loadStorage, writeCss } from './lib/utils'
 import { OPTIONS, LOCK_PROPERTIES } from './lib/options'
 import MOOL_MANTAR from './lib/mool-mantar'
+import { withErrorFallback } from './components/ErrorFallback'
 
 import './App.css'
 
@@ -130,4 +131,4 @@ const App = () => {
 }
 
 
-export default App
+export default withErrorFallback( App )
